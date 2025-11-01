4ec8de7c2e54d7548f1b5be1ec957c104dc50027
Enric-Domenech <enric.domenech@app2u.es>
2025-11-01 13:20:54 +0100

    Increase the number of movies displayed in HomeView from 35 to 40

--- composeApp/src/commonMain/kotlin/org/lanzadera/proyectos/ui/screens/home/HomeView.kt
+++ composeApp/src/commonMain/kotlin/org/lanzadera/proyectos/ui/screens/home/HomeView.kt
@@ -80,7 +80,7 @@ fun HomeView(
             primary.sortedWith(
                 compareByDescending<Movie> { it.releaseDate }
                     .thenByDescending { it.voteCount }
-            ).take(40)
+            ).take(36)
         }
     }
 
